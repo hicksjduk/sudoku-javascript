56995091a3ac2668c2541f9953e9f8ddda35fea9
Jeremy Hicks <jeremy@thehickses.org.uk>
2023-03-20 12:44:28 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,86 @@
+const permittedValues = [1,2,3,4,5,6,7,8,9]
+const emptySquare = 0
+const gridSize = permittedValues.length
+const boxes = calcBoxes()
+
+function* solve(grid)
+{
+    const {done, value:square} = emptySquares(grid).next()
+    if (done)
+        yield grid
+    else
+        yield* solveAt(grid, square)
+}
+
+function* range(first, last, step=1)
+{
+    const test = step < 0 ? i => i >= last : i => i <= last
+    for (let n = first; test(n); n += step)
+        yield n
+}
+
+function* emptySquares(grid)
+{
+    for (let row = 0; row < grid.length; row++) 
+        for (let col = 0; col < grid[row].length; col++)
+            if (grid[row][col] == emptySquare)
+                yield [row, col]
+}
+
+function* solveAt(grid, square) {
+    for (const value of allowedValues(grid, square))
+        yield* solve(setValueAt(grid, square, value))
+}
+
+function setValueAt(grid, [row, col], value) {
+    const newRow = grid[row].map((v, i) => i == col ? value : v)
+    return grid.map((r, i) => i == row ? newRow : r)
+}
+
+function* allowedValues(grid, square) {
+    const [row, col] = square
+    const functions = [() => rowValues(grid, row),
+                       () => colValues(grid, col),
+                       () => boxValues(grid, boxContaining(square))]
+    const blockedValues = new Set(functions.flatMap(f => f()))
+    for (v of permittedValues.filter(n => !blockedValues.has(n)))
+        yield v
+}
+
+function rowValues(grid, row) {
+    return grid[row].filter(n => n != emptySquare)
+}
+
+function colValues(grid, col) {
+    return grid.map(row => row[col]).filter(n => n != emptySquare)
+}
+
+function boxValues(grid, [[topRow, leftCol], [bottomRow, rightCol]]) {
+    return grid.slice(topRow, bottomRow + 1)
+        .flatMap(r => r.slice(leftCol, rightCol + 1))
+        .filter(n => n != emptySquare)
+} 
+
+function calcBoxSize() {
+    const squareRoot = Math.sqrt(gridSize)
+    const cols = [...range(Math.ceil(squareRoot), gridSize)].find(n => gridSize % n == 0)
+    return [gridSize / cols, cols]
+}
+
+function calcBoxes() {
+    const [boxRows, boxCols] = calcBoxSize()
+    const boxTopCorners = [...range(0, gridSize - 1, boxRows)].flatMap(row => 
+        [...range(0, gridSize - 1, boxCols)].map(col => [row, col])
+    )
+    return boxTopCorners.map(([topRow, leftCol]) => 
+        [[topRow, leftCol], [topRow + boxRows - 1, leftCol + boxCols -1]]
+    )
+}
+
+function boxContaining([row, col]) {
+    return boxes.find(([[topRow, leftCol], [bottomRow, rightCol]]) =>
+        topRow <= row && bottomRow >= row && leftCol <= col && rightCol >= col
+    )
+}
+
+module.exports = {solve, range, emptySquares, setValueAt}
---
+++ index.test.js
@@ -0,0 +1,68 @@
+
+
+const {solve, range, emptySquares, setValueAt} = require("./index")
+
+describe("Solve", () => {
+    test("Solvable", () => {
+        const expected = [
+            [8, 1, 2, 7, 5, 3, 6, 4, 9],
+            [9, 4, 3, 6, 8, 2, 1, 7, 5],
+            [6, 7, 5, 4, 9, 1, 2, 8, 3],
+            [1, 5, 4, 2, 3, 7, 8, 9, 6],
+            [3, 6, 9, 8, 4, 5, 7, 2, 1],
+            [2, 8, 7, 1, 6, 9, 5, 3, 4],
+            [5, 2, 1, 9, 7, 4, 3, 6, 8],
+            [4, 3, 8, 5, 2, 6, 9, 1, 7],
+            [7, 9, 6, 3, 1, 8, 4, 5, 2]]
+        const {value:solution} = solve(puzzle).next()
+        expect(solution).toStrictEqual(expected)
+    })
+    test("Not solvable", () => {
+        const {done} = solve(setValueAt(puzzle, [8,8], 3)).next()
+        expect(done).toBeTrue
+    })
+})
+
+describe("Range", () => {
+    test("Ascending", () => {
+        expect([...range(1,10,2)]).toStrictEqual([1,3,5,7,9])
+    })
+
+    test("Ascending, first > second", () => {
+        expect([...range(10,1,2)]).toStrictEqual([])
+    })
+
+    test("Descending", () => {
+        expect([...range(1,-10,-2)]).toStrictEqual([1,-1,-3,-5,-7,-9])
+    })
+
+    test("Descending, first < second", () => {
+        expect([...range(1,10,-2)]).toStrictEqual([])
+    })
+})
+
+const puzzle = [
+    [8,0,0,0,0,0,0,0,0],
+    [0,0,3,6,0,0,0,0,0],
+    [0,7,0,0,9,0,2,0,0],
+    [0,5,0,0,0,7,0,0,0],
+    [0,0,0,0,4,5,7,0,0],
+    [0,0,0,1,0,0,0,3,0],
+    [0,0,1,0,0,0,0,6,8],
+    [0,0,8,5,0,0,0,1,0],
+    [0,9,0,0,0,0,4,0,0]
+]
+
+describe("emptySquares", () => {
+    test("Find first", () => {
+        const {done, value} = emptySquares(puzzle).next()
+        expect(done).toBeTrue
+        expect(value).toBeUndefined
+    })
+    test("No empties", () => {
+        const fullPuzzle = [...range(1, 9)].map(x => [...range(1, 9)].map(y => 1))
+        const {done, value} = emptySquares(fullPuzzle).next()
+        expect(done).toBeTrue
+        expect(value).toBeUndefined
+    })
+})
